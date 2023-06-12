9aa975ebf13646800843beb485a432f733204366
Vadim P. (Vatson) <58036174+VatsonP@users.noreply.github.com>
2023-06-12 19:02:12 +0300

    Add Swagger Documentation (part 04)

--- FlightPlanApi/Controllers/FlightPlanController.cs
+++ FlightPlanApi/Controllers/FlightPlanController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FlightPlanApi.Data;
+using FlightPlanApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using FlightPlanApi.Data;
-using FlightPlanApi.Models;
-
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
 
 namespace FlightPlanApi.Controllers
 {
@@ -20,6 +20,7 @@ namespace FlightPlanApi.Controllers
 
         [HttpGet]
         [Authorize]
+        [SwaggerResponse((int)HttpStatusCode.NoContent, "no Flight plans have been filed with this system")]
         public async Task<IActionResult> FlightPlanList()
         { 
             var flightPlanList = await _database.GetAllFlightPlans();
@@ -47,6 +48,34 @@ namespace FlightPlanApi.Controllers
             return Ok(flightPlanById);
         }
 
+        /// <summary>
+        /// Files a new flight plan with the system
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/v1/flightplan/file
+        ///     {
+        ///         "aircraft_identification": "N67SVS",
+        ///         "aircraft_type": "PA-34 Piper Seneca",
+        ///         "airspeed": 128,
+        ///         "altitude": 12000,
+        ///         "flight_type": "VFR",
+        ///         "fuel_hours": 3,
+        ///         "fuel_minutes": 41,
+        ///         "departure_time": "2022-07-08T00:26:45Z",
+        ///         "estimated_arrival_time": "2022-07-08T03:49:45Z",
+        ///         "departing_airport": "KBXA",
+        ///         "arrival_airport": "KNZY",
+        ///         "route": "KBXA JOH J46 DMDUP J46 KNZY",
+        ///         "remarks": "",
+        ///         "number_onboard": 4
+        ///     }
+        /// </remarks>
+        /// <param name="flightPlan">The fight plan data to be filed.</param>
+        /// <response code="400">There is a problem with the flight plan data received by this system</response>
+        /// <response code="500">The flight plan is valid but this system cannot process it</response>
+        /// <returns></returns>
         [HttpPost]
         [Authorize]
         [Route("file")]
